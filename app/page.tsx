3d4e8cc28bfbdcd0531599ed5a3ca1f41dabada5
Julian Nymark <julian.nymark@nav.no>
2025-02-11 12:40:51 +0100

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,103 @@
-import Image from "next/image";
+import {
+  Accordion,
+  Alert,
+  Box,
+  Button,
+  Heading,
+  Page,
+  VStack,
+} from "@navikt/ds-react";
+import {
+  AccordionContent,
+  AccordionHeader,
+  AccordionItem,
+} from "@navikt/ds-react/Accordion";
+import { BoxNew } from "@navikt/ds-react/Box";
+import { PageBlock } from "@navikt/ds-react/Page";
+import { Theme } from "@navikt/ds-react/Theme";
+import { ReactNode } from "react";
+
+const MyCard = ({ children }: { children: ReactNode }) => {
+  return (
+    <Box shadow="medium" padding="5" borderRadius="xlarge" width="fit-content">
+      {children}
+    </Box>
+  );
+};
+
+const MyNewCard = ({ children }: { children: ReactNode }) => {
+  return (
+    <BoxNew
+      shadow="dialog"
+      padding="space-20"
+      borderRadius="xlarge"
+      borderColor="success"
+      borderWidth="2"
+      width="fit-content"
+    >
+      {children}
+    </BoxNew>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <div
+        className="dark css-only"
+        style={{
+          backgroundColor: "var(--ax-bg-default)",
+          color: "var(--ax-text-neutral)",
+        }}
+      >
+        <MyCard>old dog</MyCard>
+        <MyCard>old cat</MyCard>
+        <VStack gap="8">
+          <MyNewCard>dog</MyNewCard>
+          <MyNewCard>cat</MyNewCard>
+          <MyNewCard>bird</MyNewCard>
+          <MyNewCard>generic animal</MyNewCard>
+        </VStack>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Theme theme="dark">
+        <Page>
+          <PageBlock width="xl" gutters>
+            <VStack gap="8">
+              <Heading level="1" size="xlarge">
+                My app
+              </Heading>
+              <div>
+                <Alert variant="success">Alert: Success</Alert>
+              </div>
+              <div>
+                <Button variant="primary">I am a button</Button>
+                <Accordion>
+                  <AccordionItem>
+                    <AccordionHeader>Section 1</AccordionHeader>
+                    <AccordionContent>Content 1</AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionHeader>Section 2</AccordionHeader>
+                    <AccordionContent>Content 2</AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+              <div data-color-role="meta-purple">
+                <Button variant="primary">I am a button</Button>
+              </div>
+              <MyCard>old dog</MyCard>
+              <MyCard>old cat</MyCard>
+              <VStack gap="8">
+                <MyNewCard>dog</MyNewCard>
+                <MyNewCard>cat</MyNewCard>
+                <MyNewCard>bird</MyNewCard>
+                <MyNewCard>generic animal</MyNewCard>
+              </VStack>
+            </VStack>
+          </PageBlock>
+        </Page>
+      </Theme>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["@navikt/ds-react", "@navikt/aksel-icons"],
+  },
 };
 
 export default nextConfig;
